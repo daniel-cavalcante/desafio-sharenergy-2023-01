8815bbaea57777739dec722381846da79b6423ad
Daniel Cavalcante <danielcavalcante@protonmail.com>
2023-01-02 13:51:58 -0300

    Added a get method for the cat images.
    This method return a cat image if it exists on the external API,
    otherwise it returns a different imagem with a 404 message. It also
    handles the case when the external API is unavailable.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import path from 'path';
+import axios, { Axios, AxiosError } from 'axios';
 import bodyParser from 'body-parser';
 import express, { Application, Request, Response } from 'express';
 import { RandomUserGeneratorResponse } from './clients/randomUser';
@@ -34,6 +35,36 @@ app.get(
       return res.status(200).send(userList);
     } else {
       return res.status(200).send({ error: 'Query is not a string!' });
+    }
+  }
+);
+
+app.get(
+  '/api/v1/status-code/:code',
+  async (req: Request, res: Response): Promise<void> => {
+    const url = `https://http.cat/${req.params.code}`;
+
+    try {
+      const response = await axios.get(url, {
+        responseType: 'arraybuffer',
+      });
+
+      const image = Buffer.from(response.data, 'binary');
+
+      res.writeHead(200, {
+        'Content-Type': 'image/png',
+        'Content-Length': image.length,
+      });
+
+      res.end(image);
+    } catch (error: unknown) {
+      if (error instanceof AxiosError && error.response?.status === 404) {
+        const file = path.join(__dirname, '/util/404.jpeg');
+        return res.status(200).sendFile(file);
+      } else {
+        res.status(500).send({ error: 'Internal server error.' });
+        console.error((error as AxiosError).cause);
+      }
     }
   }
 );
